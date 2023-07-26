ebeb4387c3911437eaacd152c23faf23ec9a715f
Pheodaron <grindar10101@gmail.com>
2023-07-26 16:37:59 +0300

    init commit

---
+++ src/main/java/com/study/usersbase/UsersBaseApplication.java
@@ -0,0 +1,13 @@
+package com.study.usersbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/usersbase/UsersBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.usersbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
